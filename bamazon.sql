1195e2d01368bce5fa6f19eeca6f0951692ce5ba
DanLaZ <lazenberry_daniel@yahoo.com>
2019-07-16 21:35:55 -0400

    Working on the prompt path for the app. && need to later connect the user input with the data base

--- bamazon.sql
+++ bamazon.sql
@@ -13,34 +13,34 @@ CREATE TABLE products (
     PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Guppy", "Fishies", 5, 88);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Oscar", "Fishies", 7, 45);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Zebra Danios", "Fishies", 15, 35);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Cherry Barb", "Fishies", 15, 40);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Swordtails", "Fishies", 25, 27);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Killifish", "Fishies", 45, 20);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Betta", "Fishies", 5, 125);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Rainbowfish", "Fishies", 10, 65);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Angelfish", "Fishies", 55, 20);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Goldfish", "Fishies", 2, 280);
 
 SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,16 @@
-var inquirer = require("inquirer");
+var inquirer = require('inquirer');
+const chalk = require('chalk');
+var CLI = require('clui'),
+clc = require('cli-color');
+var figlet = require('figlet');
 require("dotenv").config();
 
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : "localhost",
   user     : "root",
-  password : process.env.dbPassword
+  password : process.env.dbPassword,
+  database : "bamazon"
 });
  
 connection.connect(function(err) {
@@ -14,17 +19,111 @@ connection.connect(function(err) {
     return;
   }
  
-  console.log('connected as id ' + connection.threadId);
+  // console.log('connected as id ' + connection.threadId + "\n");
 });
+  
+  // connection.end(); 
 
+/* App Header */  
+console.log(
+  chalk.magenta(
+    figlet.textSync('Bamazon Online Fish Store!', { horizontalLayout: 'full' })
+  )
+);
 
-inquirer
+/* Functions */
+
+var postProducts = function() {
+  
+ 
+
+  inquirer
   .prompt([
-      {
-        type: "input",
-        name: "userInput",
-        message: "Which item would you like to buy? Type in the id number"
+    {
+      type: "list",
+      name: "userInput",
+      message: "Are you here to purchase fish?",
+      choices: ["Yes", "No"]
+  
     }
   ]).then(function(id) {
-    console.log(id.userInput);
+    if(id.userInput === "Yes") {
+
+      let sql = `SELECT * FROM products`;
+      connection.query(sql, (error, results, fields) => {
+        if (error) {
+          return console.error(error.message);
+        }
+    
+        for (var i = 0; i < results.length; i++) { 
+          var id = chalk.white(results[i].item_id);
+          var name = chalk.yellow(results[i].product_name);
+          var spacer = "\n";
+          var line = chalk.white.bgWhite("===============================");
+          console.log(spacer + line + spacer + chalk.white("Id #") + id + " " + "- " + name + spacer + line + spacer);
+        }
+    
+      });
+      
+      console.log(chalk.bold.red("Type the id number of the fish you want"));
+
+      inquirer.prompt([
+        {
+          type: "number",
+          name: "idInput",
+          message: "Id #"
+      
+        }
+      ]).then(function(id) {
+        let sql = `SELECT * FROM products`;
+        connection.query(sql, (error, results, fields) => {
+          if (error) {
+            return console.error(error.message);
+          }
+
+          var resultIndex = id.idInput - 1;
+          var userChoice = results[resultIndex];
+        
+          var price = chalk.green(userChoice.price);
+          var quantity = chalk.red(userChoice.stock_quantity);
+          var name = chalk.yellow(userChoice.product_name);
+          var spacer = "\n";
+          var line = chalk.white.bgWhite("===============================");
+          console.log(spacer + line + spacer + name + spacer + chalk.green("$") + price + spacer + chalk.blue("Stock Amount:") + quantity + spacer + line + spacer);
+         
+        });
+
+        
+        inquirer.prompt([
+          {
+            type: "number",
+            name: "fishAmount",
+            message: "How many fish would you like to buy?"
+        
+          }
+        ]).then(function(id) {
+          var userOrder = id.fishAmount;
+          connection.query(sql, (error, results, fields) => {
+            if (error) {
+              return console.error(error.message);
+            }
+
+            var stockDifference = results[0].stock_quantity - userOrder;
+            console.log(stockDifference);
+            
+          });
+          
+  
+        });
+
+      });
+    }
+
+    else {
+      connection.end();
+    }
   });
+  
+}
+
+postProducts();
